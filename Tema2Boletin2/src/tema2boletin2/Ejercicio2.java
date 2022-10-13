0539613b4126f2eb7985cc5fd997b41744dac227
Marcosdg1221 <114072151+Marcosdg1221@users.noreply.github.com>
2022-10-13 13:30:59 +0200

    Update Ejercicio2.java

--- Tema2Boletin2/src/tema2boletin2/Ejercicio2.java
+++ Tema2Boletin2/src/tema2boletin2/Ejercicio2.java
@@ -18,9 +18,12 @@ public class Ejercicio2 {
 
 		System.out.println("Introduzca un número de día de la semana");
 
-		//Lo que introduzca será su valor
+		//Lo que introduzca será su valor en numérico
 
 		dia = all.nextInt();
+		
+		//Introduzco un switch; en caso de que el día sea 1, saldrá que es lunes.
+		//Si es 2, martes, 3 es miércoles, si es 4, será jueves, 5 es viernes, 6 sábado y 7 domingo
 		
 		switch (dia) {
 		
@@ -30,19 +33,19 @@ public class Ejercicio2 {
 		case 2:
 			System.out.println("Es martes");
 			break;
-			case 3:
+		case 3:
 			System.out.println("Es miércoles");
 			break;
-			case 4:
+		case 4:
 			System.out.println("Es jueves");
 			break;
-			case 5:
+		case 5:
 			System.out.println("Es viernes");
 			break;
-			case 6:
+		case 6:
 			System.out.println("Es sábado");
 			break;
-			case 7:
+		case 7:
 			System.out.println("Es domingo");
 			break;
 		}
